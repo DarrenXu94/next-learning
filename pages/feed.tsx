77c321086cdc3bd142b726f66a1d8d692c2f127a
Darren Xu <darrenxu1994@gmail.com>
2021-10-04 20:11:34 +1100

    bg indigo color changes

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -22,7 +22,7 @@ export default function Feed({}: feedProps) {
   }
 
   return (
-    <div className="w-full bg-white p-12 max-w-screen-lg m-auto">
+    <div className="w-full bg-white p-12 max-w-screen-lg m-auto rounded">
       <div className="header flex items-end justify-between mb-12 flex-col md:flex-row">
         <div className="title">
           <p className="text-4xl font-bold text-gray-800 mb-4">Your Newsfeed</p>
