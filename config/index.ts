97c5f4e8e4647878a40fa612a3ea35dd62eed4c0
Darren Xu <darrenxu1994@gmail.com>
2021-10-18 18:11:12 +1100

    Changed URI for db

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://your_deployment.server.com";
+  : "https://next-learning-8r04szxqk-darrenxu94.vercel.app";
