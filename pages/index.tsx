e8862208669da4aaa4ac3dea99647197f63e3819
Darren Xu <darrenxu1994@gmail.com>
2021-10-19 09:26:19 +1100

    Removing static props to see if posts render

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,11 @@ import useSession from "../lib/useSession";
 import Layout from "../components/Layout";
 import { getAllPostsAPI } from "../services/post";
 import PostRoll from "../components/PostRoll";
+import useGetAllPosts from "../lib/useGetAllPosts";
 
-const Home = ({ posts }) => {
+const Home = () => {
+  const { posts, error } = useGetAllPosts();
+
   return (
     <Layout title="NextJS Blog" description="See the latest posts">
       <div className="header flex items-end justify-between mb-12 flex-col md:flex-row">
@@ -21,14 +24,14 @@ const Home = ({ posts }) => {
 
 export default Home;
 
-export async function getStaticProps(context) {
-  try {
-    const res = await getAllPostsAPI();
+// export async function getStaticProps(context) {
+//   try {
+//     const res = await getAllPostsAPI();
 
-    return {
-      props: { posts: res.body, revalidate: 10 }, // will be passed to the page component as props
-    };
-  } catch (e) {
-    return { posts: null };
-  }
-}
+//     return {
+//       props: { posts: res.body, revalidate: 10 }, // will be passed to the page component as props
+//     };
+//   } catch (e) {
+//     return { posts: null };
+//   }
+// }
